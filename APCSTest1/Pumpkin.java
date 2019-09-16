e1e81d1ba039ed7bea28f9000efa0ecc20ab2a64
795317 <54371380+795317@users.noreply.github.com>
2019-09-16 00:28:17 -0700

    APCSTest1

---
+++ APCSTest1/Pumpkin.java
@@ -0,0 +1,20 @@
+
+/**
+ * Class Pumpkin here.
+ *
+ * @author (Nikhitha)
+ * @version (915)
+ */
+public class Pumpkin
+{
+    //Problem 2
+   private int num;
+   public Pumpkin(int n){
+     num = n;
+   }
+   
+   public static void main(String args[]){
+       Pumpkin p  = new Pumpkin(3);
+       System.out.println(p.num);
+    }
+}
