76881f44922173388bb2d78791bfb14c4bf91850
795317 <54371380+795317@users.noreply.github.com>
2019-09-20 10:27:56 -0700

    Add files via upload

---
+++ SentenceBuilder.java
@@ -0,0 +1,33 @@
+
+/**
+ * Class SentenceBuilder
+ *
+ * @author (Nikhitha Nair)
+ * @version (920)
+ */
+public class SentenceBuilder
+{
+    String determinerList[] = {"the", "a", "he", "she","this", "that", "their", "my","your"};
+    String adjectiveList [] = {"pretty", "little", "amazing", "hard", "soft", "fluffy", "glowing"};
+    String nounsList [] = {"dog", "cat", "boy", "girl", "ball", "Earth", "frog", "candy"};
+    
+    public SentenceBuilder()
+    {
+
+    }
+
+    public void loadSentences()
+    {
+    }
+    
+    public String getNounPhrase()
+    {
+        String noun = nounsList[1];
+        String adjective = adjectiveList[1];
+        String determiner = determinerList[1];
+        String nounPhrase = determiner + " " + adjective + " " + noun;
+        return nounPhrase;
+    }
+    
+}
+
