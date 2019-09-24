346f9158bff9047d7e6718b9082afda49c6402d9
795317 <54371380+795317@users.noreply.github.com>
2019-09-24 10:29:44 -0700

    v3

--- SentenceBuilder/SentenceBuilder.java
+++ SentenceBuilder/SentenceBuilder.java
@@ -3,21 +3,20 @@
  * Class SentenceBuilder
  *
  * @author (Nikhitha Nair)
- * @version (924)
+ * @version (920)
  */
 public class SentenceBuilder
 {
     String determinerList[] = {"the", "a", "he", "she","this", "that", "their", "my","your"};
     String adjectiveList [] = {"pretty", "little", "amazing", "hard", "soft", "fluffy", "glowing"};
     String nounsList [] = {"dog", "cat", "boy", "girl", "ball", "Earth", "frog", "candy"};
-    
+    String prepositionList [] = {"of", "towards", "upon", "to", "in", "out", "over", "under"};
+    String adverbList[] = {"abnormally", "bashfully", "calmly", "daily", "eagerly", "fairly", "gently"};
+    String verbList[] = {"walk", "run", "sing", "dance", "jump", "grab", "draw", "read"};
+
     public SentenceBuilder()
     {
 
-    }
-
-    public void loadSentences()
-    {
     }
     
     public String getNounPhrase()
@@ -29,13 +28,25 @@ public class SentenceBuilder
         return nounPhrase;
     }
     
+    public String getVerbPhrase()
+    {
+        String adverb = adverbList[1];
+        String verb = verbList[1];
+        String verbPhrase = adverb + verb;
+        return verbPhrase;
+    }
+    
     public String getPrepositionalPhrase()
     {
-       String preposition = "";
+       String preposition = prepositionList[1];
        String prepositionalPhrase = getNounPhrase() + " " + preposition; 
        return prepositionalPhrase;
     }
     
+    public void loadSentences()
+    {
+      String sentence = getNounPhrase() + " " + getVerbPhrase() + " " + getPrepositionalPhrase();
+    }
     
 }
 
