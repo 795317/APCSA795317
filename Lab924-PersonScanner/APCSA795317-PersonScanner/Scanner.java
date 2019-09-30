ad2867021c3988cb0165b9e5379d97df868390c3
Nikhitha Nair <795317@seq.org>
2019-09-29 20:49:12 -0700

    Person
    Person Parsor

---
+++ Lab924-PersonScanner/APCSA795317-PersonScanner/Scanner.java
@@ -0,0 +1,49 @@
+
+/**
+ * Write a description of class Scanner here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Scanner
+{
+   
+    /**
+     * Constructor for objects of class Scanner
+     */
+    public Scanner()
+    {
+        // initialise instance variables
+        
+    }
+
+    public void createPerson(String fullName){
+        
+        System.out.print('\u000C');    
+          
+        String firstName="",middleName="",lastName="";
+         if(fullName.indexOf(",") != -1){
+            //LastNameformat            
+            lastName  = fullName.substring(0,fullName.indexOf(",")); 
+            String firstMiddle = fullName.substring(fullName.indexOf(",")+2,fullName.length()); 
+           
+            if (firstMiddle.lastIndexOf(" ") != -1){               
+               firstName = firstMiddle.substring(0, firstMiddle.lastIndexOf(" "));
+               middleName  = fullName.substring(fullName.lastIndexOf(" "));
+            }else{
+                firstName = firstMiddle; 
+            }
+             
+             
+        }else{
+             firstName = fullName.substring(0,fullName.indexOf(" ")); 
+             if(fullName.indexOf(" ") != fullName.lastIndexOf(" ")){
+                 middleName =fullName.substring(fullName.indexOf(" "),fullName.lastIndexOf(" "));
+                  
+             }
+             lastName =  fullName.substring(fullName.lastIndexOf(" ")); 
+            //First Nameformat
+        }
+        Person p  = new Person(firstName,middleName,lastName);
+    }
+}
