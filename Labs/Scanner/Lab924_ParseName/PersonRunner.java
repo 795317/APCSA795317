fd0fa1c0d4b2148eb140b9284c104ea999173993
795317 <54371380+795317@users.noreply.github.com>
2019-10-02 10:21:31 -0700

    V1

---
+++ Labs/Scanner/Lab924_ParseName/PersonRunner.java
@@ -0,0 +1,21 @@
+
+/**
+ * Class PersonRunner
+ *
+ * @author (Nikihitha)
+ * @version (10.2)
+ */
+public class PersonRunner
+{
+    
+    public PersonRunner()
+    {
+       
+    }
+
+    
+    public void parseName(String name)
+    {
+      int si = name.indexOf("");
+    }
+}
