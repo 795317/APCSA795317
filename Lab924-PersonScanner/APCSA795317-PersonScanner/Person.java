e15d0815d7107770cf55fe4cfd20a0daaa51ec4e
795317 <54371380+795317@users.noreply.github.com>
2019-10-02 09:57:40 -0700

    V2

--- Lab924-PersonScanner/APCSA795317-PersonScanner/Person.java
+++ Lab924-PersonScanner/APCSA795317-PersonScanner/Person.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class Person here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Nikhitha Nair)
+ * @version (924)
  */
 public class Person
 {
