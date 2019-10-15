bf6039f3bc2411433e3a31f68133ad10b9a85828
795317 <54371380+795317@users.noreply.github.com>
2019-10-15 10:46:13 -0700

    V1

---
+++ Lab1015-MMM/MMM.java
@@ -0,0 +1,26 @@
+
+/**
+ * Class MMM
+ *
+ * @author (Nikhitha)
+ * @version (1015)
+ */
+import java.util.ArrayList;
+public class MMM
+{
+    
+    /**
+     * Constructor for objects of class MMM
+     */
+    public MMM()
+    {
+      ArrayList<Integer>nums;
+      nums = new ArrayList<Integer>();
+      
+    }
+    
+    public void loadArraylist(){
+        
+    }
+
+}
